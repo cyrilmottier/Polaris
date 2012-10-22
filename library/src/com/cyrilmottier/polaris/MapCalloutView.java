bffaaff656849fab06da8a9834b0c17835f6cab0
Cyril Mottier <cyrilmottier@gmail.com>
2012-10-22 18:24:54 +0200

    Remove a useless import (fixes 746eac03042ae25134a65159aa54f57e2e4c0b66)

--- library/src/com/cyrilmottier/polaris/MapCalloutView.java
+++ library/src/com/cyrilmottier/polaris/MapCalloutView.java
@@ -31,7 +31,6 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
